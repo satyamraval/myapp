3744596b2c0507df54bab1355bd944389c80cfbe
satyam <mail@satyamraval.com>
2017-03-30 18:19:53 +0530

    first

--- myapp/app/src/main/java/com/satyam/myapp/LoginActivity.java
+++ myapp/app/src/main/java/com/satyam/myapp/LoginActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -20,6 +21,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -52,7 +54,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "aaa@aaa.com:aaaa", "bar@example.com:world", "a@a.c"
+            "aaa@aaa.com:aaaa", "bar@example.com:world", "a@a.c:aaaa"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -314,6 +316,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             try {
                 // Simulate network access.
+                Log.d("login acivity -- ", "doInBackground");
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 return false;
@@ -337,8 +340,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                common.AlertMessage("Authorized...", mContext);
-                //finish();
+                Log.d("login acivity -- ", "onPostExecute");
+                //common.AlertMessage("Authorized...", mContext);
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+                LoginActivity.this.finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
